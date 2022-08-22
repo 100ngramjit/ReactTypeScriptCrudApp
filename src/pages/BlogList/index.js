64b5b7dec6498663d134b5ebdeda4554db989444
Sangramjit <sangramjit.dutta@aurigait.com>
2022-08-22 15:44:30 +0530

    sass added in blogs component

--- src/pages/BlogList/index.js
+++ src/pages/BlogList/index.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import "./index.scss";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { LABEL_BLOGS } from "constants/Constants";
 import { baseURL } from "api_urls/ApiLinks";
 import Skeleton from "react-loading-skeleton";
-import { Container, Card, CardGroup, Col, Row } from "react-bootstrap";
+import { Container, Card, Col, Row } from "react-bootstrap";
 
 const BlogList = () => {
   const blogs = useSelector((state) => state);
@@ -25,22 +26,18 @@ const BlogList = () => {
     dispatch(getData());
   }, []);
   return (
-    <Container>
+    <Container id="bloglist">
       <h3 className="text-light">{LABEL_BLOGS}</h3>
-      <Row xs={1} md={2} lg={3} className="g-4">
+      <Row xs={1} md={2} lg={3}>
         {blogs.data.data ? (
           blogs.data.data.map(
             ({ title, details, id }) =>
               details && (
                 <Col key={id}>
-                  <Card
-                    style={{ width: "20rem", margin: "1rem" }}
-                    bg="dark"
-                    text="light"
-                  >
+                  <Card id="card" bg="dark" text="light">
                     <Card.Body>
                       <Card.Title>{title}</Card.Title>
-                      <hr />
+                      <hr id="divider" />
                       <Card.Text>
                         {details.substr(0, 250) + "....."}
                       </Card.Text>{" "}
